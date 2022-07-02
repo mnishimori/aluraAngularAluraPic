03d76b8535d54abb24a508c387c6d2f8172cc764
mnishimori <mnishimori@yahoo.com.br>
2022-07-02 19:31:47 -0300

    ErrorsModule e NotFoundComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 
 import {PhotosModule} from './photos/photos.module';
+import {ErrorsModule} from './errors/errors.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import {PhotosModule} from './photos/photos.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ErrorsModule,
     PhotosModule
   ],
   providers: [],

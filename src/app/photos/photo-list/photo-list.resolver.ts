e93456045b8b4079caed5efe06cb40db86a821f8
mnishimori <mnishimori@yahoo.com.br>
2022-07-08 22:51:53 -0300

    Reorganizção de imports

--- src/app/photos/photo-list/photo-list.resolver.ts
+++ src/app/photos/photo-list/photo-list.resolver.ts
@@ -1,10 +1,6 @@
-import { Injectable } from '@angular/core';
-import {
-  Router, Resolve,
-  RouterStateSnapshot,
-  ActivatedRouteSnapshot
-} from '@angular/router';
-import { Observable, of } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
+import {Observable} from 'rxjs';
 
 import {PhotoService} from '../photo/photo.service';
 import {Photo} from '../photo/photo';

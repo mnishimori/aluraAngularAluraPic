aea74184bd74ca2cf0d41a577963999c68a64953
mnishimori <mnishimori@yahoo.com.br>
2022-07-23 13:33:02 -0300

    feat: TokenService para manutenção do token no localStorage. Atualização do AuthService para utilizar o TokenService

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {tap} from 'rxjs/operators';
+import {TokenService} from '../token/token.service';
 
 const API_URL = 'http://localhost:3000/user/login';
 
@@ -10,12 +11,15 @@ const API_URL = 'http://localhost:3000/user/login';
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private tokenService: TokenService) { }
 
   authenticate(userName: string, password: string): Observable<any> {
     return this.http.post(API_URL, {userName, password}, { observe: 'response'})
       .pipe(tap(res => {
         const authToken = res.headers.get('x-access-token');
+        this.tokenService.setToken(authToken);
         console.log('Usuário ' + userName + ' autenticado com o token ' + authToken);
       }));
   }

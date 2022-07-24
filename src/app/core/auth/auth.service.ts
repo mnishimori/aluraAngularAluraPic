8774fc6b40e0c96715617d409d6b8524a2d8dde7
mnishimori <mnishimori@yahoo.com.br>
2022-07-24 14:30:24 -0300

    feat: User, UserService utilizado para solicitar o armazenamento do token em localStorage, decodificar o token e converter em User. Em AuthService, foi alterada a classe que realiza a solicitação de armazenamento do token de TokenService para UserService

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {tap} from 'rxjs/operators';
-import {TokenService} from '../token/token.service';
+import {UserService} from '../user/user.service';
 
 const API_URL = 'http://localhost:3000/user/login';
 
@@ -13,14 +13,13 @@ export class AuthService {
 
   constructor(
     private http: HttpClient,
-    private tokenService: TokenService) { }
+    private userService: UserService) { }
 
   authenticate(userName: string, password: string): Observable<any> {
     return this.http.post(API_URL, {userName, password}, { observe: 'response'})
       .pipe(tap(res => {
         const authToken = res.headers.get('x-access-token');
-        this.tokenService.setToken(authToken);
-        console.log('Usuário ' + userName + ' autenticado com o token ' + authToken);
+        this.userService.setToken(authToken);
       }));
   }
 }
---
+++ src/app/core/user/user.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {TokenService} from '../token/token.service';
+import {Subject} from 'rxjs';
+import {User} from './user';
+import * as jwt_decode from 'jwt-decode';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private userSubject = new Subject<User>();
+
+  constructor(private tokenService: TokenService) {
+
+    this.tokenService.hasToken() && this.decodeAndNotify();
+  }
+
+  getUser(){
+    this.userSubject.asObservable();
+  }
+
+  public setToken(token: string): void {
+    this.tokenService.setToken(token);
+    this.decodeAndNotify();
+  }
+
+  private decodeAndNotify(): void {
+    const token = this.tokenService.getToken();
+    const user = jwt_decode.decode(token) as User;
+    this.userSubject.next(user);
+  }
+}

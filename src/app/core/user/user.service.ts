fe8f5d19ee1f0f415bd4cbd1ff0863afa5d7ed79
mnishimori <mnishimori@yahoo.com.br>
2022-07-26 18:32:43 -0300

    feat: guarda de rotas

--- src/app/core/user/user.service.ts
+++ src/app/core/user/user.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {TokenService} from '../token/token.service';
-import {BehaviorSubject} from 'rxjs';
+import {BehaviorSubject, Observable} from 'rxjs';
 import {User} from './user';
 import jwt_decode from 'jwt-decode';
 
@@ -10,13 +10,14 @@ import jwt_decode from 'jwt-decode';
 export class UserService {
 
   private userSubject = new BehaviorSubject<User>(null);
+  private userName: string;
 
   constructor(private tokenService: TokenService) {
 
     this.tokenService.hasToken() && this.decodeAndNotify();
   }
 
-  getUser(){
+  getUser(): Observable<User>{
     return this.userSubject.asObservable();
   }
 
@@ -25,14 +26,23 @@ export class UserService {
     this.decodeAndNotify();
   }
 
-  logout(){
+  logout(): void {
     this.tokenService.removeToken();
     this.userSubject.next(null);
+  }
+
+  logged(): boolean {
+    return this.tokenService.hasToken();
+  }
+
+  getUserName(): string {
+    return this.userName;
   }
 
   private decodeAndNotify(): void {
     const token = this.tokenService.getToken();
     const user = jwt_decode(token) as User;
+    this.userName = user.name;
     this.userSubject.next(user);
   }
 }

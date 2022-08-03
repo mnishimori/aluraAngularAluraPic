4eb2c5cffa3364e9cc092be3251470169f18cf33
mnishimori <mnishimori@yahoo.com.br>
2022-08-03 13:57:16 -0300

    feat: interceptor para capturar requisições que são enviadas para o backend e acrescentar o token da autenticação

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import {RouterModule} from '@angular/router';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {RequestInterceptor} from './auth/request.interceptor';
 
 
 
@@ -15,6 +17,13 @@ import {RouterModule} from '@angular/router';
   imports: [
     CommonModule,
     RouterModule
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: RequestInterceptor,
+      multi: true
+    }
   ]
 })
 export class CoreModule { }

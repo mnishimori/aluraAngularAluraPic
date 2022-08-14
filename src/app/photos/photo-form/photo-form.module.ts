3f5817240ddd73df97c66640bfb44b2e001ded1b
mnishimori <mnishimori@yahoo.com.br>
2022-08-13 21:38:44 -0300

    feat: Formulário de upload de fotos

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {PhotoFormComponent} from './photo-form.component';
-
-
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {VmessageModule} from '../../shared/components/vmessage/vmessage.module';
 
 @NgModule({
   declarations: [ PhotoFormComponent ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    VmessageModule
   ]
 })
 export class PhotoFormModule { }
